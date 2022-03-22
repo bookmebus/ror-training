087443811fe60c735f470017aac1c9e5c8f9170c
kimleang123 <lykimleang456@gmail.com>
2022-03-22 15:02:05 +0700

    create-picture-model

---
+++ db/migrate/20220322075845_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pictures do |t|
+      t.bigint :post_id
+      t.string :caption, null: false
+
+      t.timestamps
+    end
+    add_foreign_key :pictures, :posts, column: :post_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_322_074_743) do
+ActiveRecord::Schema[7.0].define(version: 20_220_322_075_845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 20_220_322_074_743) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index %w[user_id friend_id], name: 'index_bonds_on_user_id_and_friend_id', unique: true
+  end
+
+  create_table 'pictures', force: :cascade do |t|
+    t.bigint 'post_id'
+    t.string 'caption', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
   create_table 'posts', force: :cascade do |t|
@@ -47,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_322_074_743) do
 
   add_foreign_key 'bonds', 'users'
   add_foreign_key 'bonds', 'users', column: 'friend_id'
+  add_foreign_key 'pictures', 'posts'
   add_foreign_key 'posts', 'posts', column: 'thread_id'
   add_foreign_key 'posts', 'users'
 end
