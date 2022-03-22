89b6405fb04d40b7c81aa9cd1d61e5f5056e7cce
kimleang123 <lykimleang456@gmail.com>
2022-03-22 15:06:40 +0700

    create-status-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,51 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_322_075_845) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_080535) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bonds', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'friend_id', null: false
-    t.string 'state', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[user_id friend_id], name: 'index_bonds_on_user_id_and_friend_id', unique: true
+  create_table "bonds", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.string "state", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_bonds_on_user_id_and_friend_id", unique: true
   end
 
-  create_table 'pictures', force: :cascade do |t|
-    t.bigint 'post_id'
-    t.string 'caption', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "pictures", force: :cascade do |t|
+    t.bigint "post_id"
+    t.string "caption", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'thread_id'
-    t.string 'postable_type', null: false
-    t.bigint 'postable_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[postable_type postable_id], name: 'index_posts_on_postable'
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "thread_id"
+    t.string "postable_type", null: false
+    t.bigint "postable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["postable_type", "postable_id"], name: "index_posts_on_postable"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username', null: false
-    t.string 'first_name', null: false
-    t.string 'last_name'
-    t.string 'email', null: false
-    t.boolean 'isPublic', default: true, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  create_table "statuses", force: :cascade do |t|
+    t.string "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'bonds', 'users'
-  add_foreign_key 'bonds', 'users', column: 'friend_id'
-  add_foreign_key 'pictures', 'posts'
-  add_foreign_key 'posts', 'posts', column: 'thread_id'
-  add_foreign_key 'posts', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "first_name", null: false
+    t.string "last_name"
+    t.string "email", null: false
+    t.boolean "isPublic", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "bonds", "users"
+  add_foreign_key "bonds", "users", column: "friend_id"
+  add_foreign_key "pictures", "posts"
+  add_foreign_key "posts", "posts", column: "thread_id"
+  add_foreign_key "posts", "users"
 end
