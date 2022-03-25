f6eb88e6b347094efb765d2e54f01e2e5541b00f
Thea Choem <theacheng.g6@gmail.com>
2022-03-25 16:35:48 +0700

    fixing for migration

--- tandibi/spec/models/user_spec.rb
+++ tandibi/spec/models/user_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
 RSpec.describe User, type: :model do
-  def create_a_user(email: "#{SecureRandom.hex(4)}@example.org")
-    User.create!(
-      first_name: "Adam",
-      email: email,
-      username: SecureRandom.hex(4),
-) end
-
   describe "#valid?" do
-    it "is valid when email is unique" do
-      create_a_user
-
-      user = User.new
+    describe "is valid when email is unique" do
+      user = User.new(
+        first_name: "Adam",
+        username: SecureRandom.hex(4),
+      )
       user.email = "adam@example.org"
-
       expect(user.valid?).to be true
     end
   end
