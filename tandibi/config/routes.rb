7577a73f081b935eed49485aff9d44d772719ae7
Thea Choem <theacheng.g6@gmail.com>
2022-03-28 11:48:21 +0700

    add login & sign up buttons

--- tandibi/config/routes.rb
+++ tandibi/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_up' => 'devise/registrations/new#post'
+    get '/users/login' => 'devise/sessions/new#post'
   end
   root to: "home#index"
 end
