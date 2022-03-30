f04d6831b882dd0aa02caa8dd80d8f67042ddf01
kimleang123 <lykimleang456@gmail.com>
2022-03-30 16:09:37 +0700

    make button link to sign up, log in, log out, edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  #   get '/users/sign_up' => 'devise/registrations/new#post'
+  #   get '/users/login' => 'devise/sessions/new#post'
+  # end
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
