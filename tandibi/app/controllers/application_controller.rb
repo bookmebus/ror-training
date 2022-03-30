470014348c6ab79ddbeeaecc29c6a9e25a1fca3b
Thea Choem <theacheng.g6@gmail.com>
2022-03-30 09:38:30 +0700

    close #13 authentication (#14)
    * config devise & setup basic home view
    
    * generate devise views
    
    rails g devise:views
    
    * generate & migrate with devise user
    
    rails g devise user
    
    * setup authentication flow
    
    * render current user in home
    
    * add login & sign up buttons
    
    * deploy & update readme
    
    * add edit button

--- tandibi/app/controllers/application_controller.rb
+++ tandibi/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def devise_error_messages!
+  #   flash[:notice] = resource.errors.full_messages.first
+  #   render :action => 'devise/registrations/new'
+  # end
+
+  protected 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { 
+      |u| u.permit(:email, :username, :first_name, :last_name, :password, :password_confirmation)
+    }
+  end
 end
--- tandibi/config/routes.rb
+++ tandibi/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_up' => 'devise/registrations/new#post'
+    get '/users/login' => 'devise/sessions/new#post'
+  end
+  root to: "home#index"
 end
