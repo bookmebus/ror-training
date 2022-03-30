735d12730093edf169dedbf4277d71e74ec84e55
kimleang123 <lykimleang456@gmail.com>
2022-03-30 16:44:39 +0700

    set up timeline controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'timelines/index'
+  get 'timelines/show'
   devise_for :users
   # devise_scope :user do
   #   get '/users/sign_out' => 'devise/sessions#destroy'
