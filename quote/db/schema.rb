8d24e79b6e091073263ed895f7a3b7ce3fde714b
Thea Choem <theacheng.g6@gmail.com>
2022-03-31 17:51:55 +0700

    close #17 quote app auth (#18)
    * setup auth controller
    
    * setup auth views
    
    * fix bug on auth controllers

--- quote/db/schema.rb
+++ quote/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_093309) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_094214) do
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "v_quotes", force: :cascade do |t|
     t.text "quote"
     t.text "authur"
